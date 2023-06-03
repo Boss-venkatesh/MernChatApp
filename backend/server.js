220c500b6c307e2a1e5d7c8e06468746b25f6c6a
venkat <88572917+Boss-venkatesh@users.noreply.github.com>
2023-06-03 14:04:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const server = require('http').createServer(app);
 const PORT = 5001;
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'https://mernchatapp-frontend.onrender.com',
+    origin: 'https://mernchatapp-frnt.onrender.com',
     methods: ['GET', 'POST']
   }
 })
